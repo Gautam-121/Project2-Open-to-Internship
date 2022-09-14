b31fffe7a1ec7ff4c9e28c7aae772a8ccb430403
Gautam <gautamdhakate1234@gmail.com>
2022-09-14 12:42:26 +0530

    update collegeController

---
+++ src/Controllers/collegeController.js
@@ -0,0 +1,54 @@
+const createCollege = async function (req, res) {
+
+    try {
+
+        const isValidRequestBody = req.body
+
+        if (Object.keys(isValidRequestBody).length == 0) {
+            return res.status(400).send({ msg: "No paramerter found , please provide college detail", status: false })
+        }
+
+        const { name, fullName, logoLink } = req.body
+
+        if (!name) {
+            return res.status(400).send({ msg: "Name is required", status: false })
+        }
+        if (!/^[a-z]{2,100}$/i.test(name)) {
+            res.status(400).send({ msg: "Name should contain letters only and it between 2 to 100", status: false })
+        }
+
+        const isNameAlready = await collegeModel.findOne({ name: name })
+
+        if (isNameAlready) {
+            return res.status(400).send({ msg: `${isNameAlready} is already register`, status: false })
+        }
+
+        if (!fullName) {
+            return res.status(400).send({ msg: "FullName is required", status: false })
+        }
+        if (!/^[a-z]{5,100}$/i.test(fullName)) {
+            res.status(400).send({ msg: "Name should contain letters only and it between 2 to 100", status: false })
+        }
+
+        const isfullNameAlready = await collegeModel.findOne({ name: name })
+
+        if (isfullNameAlready) {
+            return res.status(400).send({ msg: `${isfullNameAlready} is already register `, status: false })
+        }
+
+        if (!logoLink) {
+            return res.status(400).send({ msg: "Logi link is required", status: false })
+        }
+
+        const collegeDetail = { name, fullName, logoLink }
+
+        const collegeCreate = await collegeModel.create(collegeDetail)
+
+        res.status(201).send({ status: true, msg: "college registration succesufully created", data: collegeCreate })
+
+    } catch (err) {
+        return res.status(500).send({ msg: err.message, status: false })
+    }
+}
+
+module.exports.createCollege = createCollege
