42d983912ba799b7b7c2a44d992d73148945f0b0
Afshan <pathanafshana0@gmail.com>
2022-09-14 12:24:14 +0530

    model

---
+++ src/models/collegeModels.js
@@ -0,0 +1,40 @@
+//---------------------------------------importing modules--------------------------------------------
+const mongoose = require("mongoose")
+
+//----------------------------------------Creating Schema---------------------------------------------
+
+
+const collegeSchema= new mongoose.schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true
+
+    },
+    logolonk: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    isDeleted: {
+        type : Boolean,
+        default: false
+
+ }
+},{timestamps:true});
+
+//---------------------------------- exporting all the model here--------------------------------------
+
+module.exports = mongoose.model("college", collegeSchema);
+
+
+// { name: {mandatory, unique, example iith},
+//  fullName: {mandatory},
+//  logolink: {mandatory},
+// isDeleted: {boolean, default: false}}
