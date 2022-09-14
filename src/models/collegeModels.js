daa9f91486a882297bb358e4ada0ff933875e23e
Afshan <pathanafshana0@gmail.com>
2022-09-14 13:26:32 +0530

    name

--- src/models/collegeModels.js
+++ src/models/collegeModels.js
@@ -11,7 +11,7 @@ const collegeSchema= new mongoose.Schema({
         unique: true,
         trim: true
     },
-    fullname: {
+    fullName: {
         type: String,
         required: true,
         trim: true
